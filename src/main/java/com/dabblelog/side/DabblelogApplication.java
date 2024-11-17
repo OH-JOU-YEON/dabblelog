2e3bb18c52030736379643d61ba269180ed9ace7
hyperclutter <hyperclutter@gmail.com>
2024-11-18 08:42:53 +0900

    start

---
+++ src/main/java/com/dabblelog/side/DabblelogApplication.java
@@ -0,0 +1,13 @@
+package com.dabblelog.side;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabblelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DabblelogApplication.class, args);
+	}
+
+}
