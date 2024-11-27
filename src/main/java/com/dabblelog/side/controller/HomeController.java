ff2b5725084f84004f03e624a2e7fcde02ecf748
hyperclutter <hyperclutter@gmail.com>
2024-11-27 14:00:14 +0900

    mapping setting url

--- src/main/java/com/dabblelog/side/controller/HomeController.java
+++ src/main/java/com/dabblelog/side/controller/HomeController.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    String mappingHome(Model model) {
+    public String mappingHome(Model model) {
+
+        //로그인 되어 있는지 검사 후에 로그인 돼 있으면 프로필 창 가져오는 메서드
+
+        //일단 트렌딩에 맞춰서 좋아요 개수가 많은 이번 주 게시물들을 불러오는 메서드
+
 
         return "basic/Home";
     }
--- src/main/java/com/dabblelog/side/controller/SettingController.java
+++ src/main/java/com/dabblelog/side/controller/SettingController.java
@@ -2,7 +2,14 @@ package com.dabblelog.side.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SettingController {
+
+    @GetMapping("/setting")
+    public String mappingSetting(Model model) {
+        return "/basic/Setting";
+    }
 }
