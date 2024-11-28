dc81b6038219316eaa86a2f595fdf9a2812574bd
hyperclutter <hyperclutter@gmail.com>
2024-11-28 16:22:02 +0900

    implement reple db

---
+++ src/main/java/com/dabblelog/side/domain/Reple.java
@@ -0,0 +1,22 @@
+package com.dabblelog.side.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Reple {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "reple_mapping_id")
+    private RepleMapping repleMapping;
+
+    private boolean replyOrNot;
+}
---
+++ src/main/java/com/dabblelog/side/domain/RepleMapping.java
@@ -0,0 +1,21 @@
+package com.dabblelog.side.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class RepleMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+
+}
