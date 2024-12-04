4904137f6fc58b09c49137a1fac6132a3a63bc6f
ohjou <hyperclutter@gmail.com>
2024-12-04 18:28:23 +0900

    make service template

---
+++ src/main/java/com/dabblelog/side/service/SettingService.java
@@ -0,0 +1,8 @@
+package com.dabblelog.side.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SettingService{
+}
---
+++ src/main/java/com/dabblelog/side/service/impl/SettingServiceImpl.java
@@ -0,0 +1,8 @@
+package com.dabblelog.side.service.impl;
+
+import com.dabblelog.side.service.SettingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SettingServiceImpl implements SettingService {
+}
