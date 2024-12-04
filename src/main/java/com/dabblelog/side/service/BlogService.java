692b33a6d3549f795e90eff5dc6586b8f086e082
ohjou <hyperclutter@gmail.com>
2024-12-04 18:53:44 +0900

    semantic error modify

--- src/main/java/com/dabblelog/side/service/BlogService.java
+++ src/main/java/com/dabblelog/side/service/BlogService.java
@@ -4,21 +4,9 @@ import com.dabblelog.side.domain.Blog;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface BlogService {
+public class BlogService {
 
-    //생성
 
-    public Blog createBlog(Long userId);
-
-    //수정
-
-    //블로그 이름 수정
-
-    public void updateBlogName(String blogName);
-
-    //삭제
-
-    public void deleteBlog(Long blogId);
 
 
 }
