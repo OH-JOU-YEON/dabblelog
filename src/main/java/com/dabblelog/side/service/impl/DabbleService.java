7dc13aded3929b526368cea2927eb45db56d2404
hyperclutter <hyperclutter@gmail.com>
2025-01-08 16:54:28 +0900

    이미지가 없을 경우 미리보기 이미지 추가

---
+++ src/main/java/com/dabblelog/side/service/impl/DabbleService.java
@@ -0,0 +1,17 @@
+package com.dabblelog.side.service.impl;
+
+
+import com.dabblelog.side.repository.DabbleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DabbleService {
+
+    @Autowired
+    DabbleRepository dabbleRepository;
+}
