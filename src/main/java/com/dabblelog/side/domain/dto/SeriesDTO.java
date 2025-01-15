1d9bb11ab520ecb90418046a30cf3adc104fef90
hyperclutter <hyperclutter@gmail.com>
2025-01-15 15:13:05 +0900

    시리즈 dto 작성

---
+++ src/main/java/com/dabblelog/side/domain/dto/SeriesDTO.java
@@ -0,0 +1,18 @@
+package com.dabblelog.side.domain.dto;
+
+
+import lombok.Getter;
+
+@Getter
+public class SeriesDTO {
+
+    private String title;
+
+    private String color;
+
+    private Long postCount;
+
+    private String lastUpdate;
+
+    private String seriesImage;
+}
