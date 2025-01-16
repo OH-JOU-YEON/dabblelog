be98186a572a78fe20cf66e0700e9408c97c5b80
hyperclutter <hyperclutter@gmail.com>
2025-01-16 17:44:12 +0900

    시리즈 페이징, 빈 페이지 문구 추가

--- src/main/java/com/dabblelog/side/controller/SeriesController.java
+++ src/main/java/com/dabblelog/side/controller/SeriesController.java
@@ -14,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,8 +34,7 @@ public class SeriesController {
     BlogRepository blogRepository;
 
 @PostMapping("/series")
-    public String seriesCreate(Model model, HttpServletRequest request, @PageableDefault(page=0, size=6, sort="seriesLastUpdateDay"
-        , direction= Sort.Direction.DESC) Pageable pageable) {
+    public String seriesCreate(Model model, HttpServletRequest request, @PageableDefault(page=0, size=6) Pageable pageable) {
 
     //세션 유저에서 유저 가져와서 유저 아이디 알아내기
 
@@ -60,14 +58,33 @@ public class SeriesController {
 
     seriesService.createSeries(blog, color, title);
 
+    String email = sessionuser.getEmail();
+
+
+    Page<getSeriesDTO> getSeriesDTOS = seriesService.getSeries(email,pageable);
+
+    //페이지블럭 처리
+    //1을 더해주는 이유는 pageable은 0부터라 1을 처리하려면 1을 더해서 시작해주어야 한다.
+    int nowPage = getSeriesDTOS.getPageable().getPageNumber() + 1;
+    //-1값이 들어가는 것을 막기 위해서 max값으로 두 개의 값을 넣고 더 큰 값을 넣어주게 된다.
+    int startPage =  Math.max(nowPage - 4, 1);
+    int endPage = Math.min(nowPage+9, getSeriesDTOS.getTotalPages());
+    model.addAttribute("nowPage",nowPage);
+    model.addAttribute("startPage", startPage);
+    model.addAttribute("endPage", endPage);
+
+
+    model.addAttribute("email", email);
+
+    model.addAttribute("seriesList",getSeriesDTOS);
+
 
 
     return "basic/Series";
 }
 
     @GetMapping("/series")
-    public String mappingSeries(Model model, HttpServletRequest request, @PageableDefault(page=0, size=6, sort="seriesLastUpdateDay"
-    , direction= Sort.Direction.DESC) Pageable pageable) {
+    public String mappingSeries(Model model, HttpServletRequest request, @PageableDefault(page=0, size=6) Pageable pageable) {
 
         HttpSession session = request.getSession(false);
 
--- src/main/java/com/dabblelog/side/domain/dto/getSeriesDTO.java
+++ src/main/java/com/dabblelog/side/domain/dto/getSeriesDTO.java
@@ -3,7 +3,9 @@ package com.dabblelog.side.domain.dto;
 
 import com.dabblelog.side.domain.Series;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Getter
 public class getSeriesDTO {
 
@@ -20,12 +22,19 @@ public class getSeriesDTO {
 
     public getSeriesDTO(Series series, Long seriesPostCount,String seriesRecentThumbnails) {
         this.seriesColor = series.getColor();
+        log.info("seriesDTO.seriesColor = {}", this.seriesColor);
 
         this.seriesTitle = series.getTitle();
 
+        log.info("seriesDTO.seriesTitle = {}", this.seriesTitle);
+
         this.seriesRecentThumbnails = seriesRecentThumbnails;
 
+        log.info("seriesDTO.seriesThumbnails = {}", this.seriesRecentThumbnails);
+
         this.seriesPostCount = seriesPostCount;
+
+        log.info("seriesDTO.seriesPostCount = {}", this.seriesPostCount);
 
 
 
