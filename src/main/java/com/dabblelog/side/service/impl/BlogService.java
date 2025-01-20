248fdca4ad31f7d0aaef6636312b272a0135588f
hyperclutter <hyperclutter@gmail.com>
2025-01-20 16:01:17 +0900

    블로그 페이지 프로필 dto 얻어오는 서비스 메서드 작성

--- src/main/java/com/dabblelog/side/service/impl/BlogService.java
+++ src/main/java/com/dabblelog/side/service/impl/BlogService.java
@@ -4,7 +4,9 @@ package com.dabblelog.side.service.impl;
 import com.dabblelog.side.domain.Blog;
 import com.dabblelog.side.domain.User;
 import com.dabblelog.side.domain.dto.getBlogPostDTO;
+import com.dabblelog.side.domain.dto.getBlogProfileDTO;
 import com.dabblelog.side.repository.BlogRepository;
+import com.dabblelog.side.repository.FollowerRepository;
 import com.dabblelog.side.repository.PostRepository;
 import com.dabblelog.side.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -29,6 +31,9 @@ public class BlogService {
     @Autowired
     PostRepository postRepository;
 
+    @Autowired
+    FollowerRepository followerRepository;
+
     //본인 블로그가 존재하는지 하지 않는지 판단 후에 없으면 새 블로그 생성함 검사하는 거 이메일
 
     @Transactional
@@ -47,5 +52,14 @@ public class BlogService {
 
         Blog blog = blogRepository.findByBlogName(blogName).get();
         return postRepository.findAllByBlogId(blog,pageable).map(getBlogPostDTO::new);
+    }
+
+    public getBlogProfileDTO getBlogProfileDTO(String blogName) {
+        Blog blog = blogRepository.findByBlogName(blogName).get();
+
+        User blogUser = blog.getUser();
+
+        return new getBlogProfileDTO(blogUser,followerRepository.countByFollowingId(blogUser),followerRepository.countByFollowedId(blogUser));
+
     }
 }
