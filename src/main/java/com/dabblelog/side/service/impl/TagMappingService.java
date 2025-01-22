53847f29851454502913a22b9dc41473e1b0dec0
hyperclutter <hyperclutter@gmail.com>
2025-01-22 17:12:55 +0900

    포스트의 태그들 이름 반환해주는 메서드 작성

--- src/main/java/com/dabblelog/side/service/impl/TagMappingService.java
+++ src/main/java/com/dabblelog/side/service/impl/TagMappingService.java
@@ -1,13 +1,17 @@
 package com.dabblelog.side.service.impl;
 
 
+import com.dabblelog.side.domain.Blog;
 import com.dabblelog.side.domain.Post;
 import com.dabblelog.side.domain.PostTag;
 import com.dabblelog.side.domain.TagMapping;
+import com.dabblelog.side.repository.PostRepository;
 import com.dabblelog.side.repository.TagMappingRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TagMappingService {
@@ -15,13 +19,26 @@ public class TagMappingService {
     @Autowired
     TagMappingRepository tagMappingRepository;
 
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    BlogService blogService;
+
     //태그 매핑 생성하는 로직
 
     public TagMapping createTagMapping(Post postId, PostTag tagId) {
         return tagMappingRepository.save(new TagMapping(tagId, postId));
     }
 
+    public List<String> getTagTitle(String blogName,String postTitle) {
 
+        Blog blog = blogService.getBlogByName(blogName);
+        Post post = postRepository.findByBlogIdAndTitle(blog,postTitle);
+
+        return tagMappingRepository.findAllByPostId(post).stream().map(s->s.getTagId().getTitle()).toList();
+
+    }
 
 
 
