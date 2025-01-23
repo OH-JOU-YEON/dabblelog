4a4d9717ebfa06a2f84a37bd283c27842764fd26
hyperclutter <hyperclutter@gmail.com>
2025-01-23 16:08:24 +0900

    댓글 컨트롤러 수정

---
+++ src/main/java/com/dabblelog/side/controller/RepleController.java
@@ -0,0 +1,15 @@
+package com.dabblelog.side.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RepleController {
+
+
+    @PostMapping("/reple/create")
+    public void createReple() {
+        //만들고 답글 dto 던져서
+    }
+}
