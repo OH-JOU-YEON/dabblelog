f6ac542ebd38d9ccdede8667b0b9d2ba6737bba2
hyperclutter <hyperclutter@gmail.com>
2025-01-27 16:02:09 +0900

    프로필 수정 자바스크립트 함수 작성

--- src/main/java/com/dabblelog/side/controller/SettingController.java
+++ src/main/java/com/dabblelog/side/controller/SettingController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SettingController {
@@ -25,6 +26,11 @@ public class SettingController {
         return "/basic/Setting";
     }
 
+    @PostMapping("/setting/profileText")
+    public void modifyProfileText() {
+
+
+    }
 
 
 
